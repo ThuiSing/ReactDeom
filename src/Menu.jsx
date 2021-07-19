78a96f8c60fa8394c9c9587c902401db47ae9bfb
ThuiSing <thuisingmong0@gmail.com>
2021-07-20 05:05:33 +0600

    bug fixed

--- src/Menu.jsx
+++ src/Menu.jsx
@@ -1,16 +1,36 @@
-import React from 'react'
-import { NavLink } from 'react-router-dom'
+import React from "react";
+import { NavLink } from "react-router-dom";
 
 const Menu = () => {
   return (
     <>
       <ul className="main_menu">
-          <li><NavLink exact activeClassName="main_active_menu" to="/"> Home </NavLink></li>
-          <li><NavLink activeClassName="main_active_menu" to="/services"> Services </NavLink></li>
-          <li><NavLink activeClassName="main_active_menu" to="/about"> About </NavLink></li>
-          <li><NavLink activeClassName="main_active_menu" to="/contact"> Contact </NavLink></li>
+        <li>
+          <NavLink exact activeClassName="main_active_menu" to="/">
+            {" "}
+            Home{" "}
+          </NavLink>
+        </li>
+        <li>
+          <NavLink exact activeClassName="main_active_menu" to="/services">
+            {" "}
+            Services{" "}
+          </NavLink>
+        </li>
+        <li>
+          <NavLink exact activeClassName="main_active_menu" to="/about">
+            {" "}
+            About{" "}
+          </NavLink>
+        </li>
+        <li>
+          <NavLink exact activeClassName="main_active_menu" to="/contact">
+            {" "}
+            Contact{" "}
+          </NavLink>
+        </li>
       </ul>
     </>
-  )
-}
+  );
+};
 export default Menu;
